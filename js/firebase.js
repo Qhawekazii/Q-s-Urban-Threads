09d824def28981a893da35dc390127f79a25e15e
Emily Maramani <emilyqm01@gmail.com>
2026-08-17 10:39:19 +0200

    added auth.js file

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,19 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDnJJBeicH16y7mHKR2GKMOFThpwVgabA0",
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
     authDomain: "q-s-urban-threads.firebaseapp.com",
     projectId: "q-s-urban-threads",
     storageBucket: "q-s-urban-threads.firebasestorage.app",
     messagingSenderId: "943762759954",
     appId: "1:943762759954:web:8337e91b6280c027d1d3cb"
-  };
- 
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log("Urban Threads is running!");
+import { auth, db } from "./firebase.js";
+
+console.log("Urban Threads Firebase connected!");
+console.log("Authentication:", auth);
+console.log("Firestore:", db);
